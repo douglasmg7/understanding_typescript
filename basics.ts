a62cb33b21785903b30f00d0d936441a08cfa009
douglasmg7 <douglasmg7@gmail.com>
2026-07-13 08:43:26 -0300

    Enumeration and union

--- basics.ts
+++ basics.ts
@@ -1,40 +1,47 @@
 // Infering types
-let val1 = 'asdf';
+let val1 = "asdf";
 
 // Explicitly defining types
 let num: number = 2;
 
 // Only defining types for the variable, not the value
 let val2: string;
-val2 = 'asdf';
+val2 = "asdf";
 
 // Using the any type
-let val3: any = 3
-val3 = 'asdf';
+let val3: any = 3;
+val3 = "asdf";
 
 // Defining types for arrays
-let val4: (string | number)[] = ['asdf', 1];
+let val4: (string | number)[] = ["asdf", 1];
 val4.push(2);
 // Using the Array generic type
-let val5: Array<string | number> = ['asdf', 1];
+let val5: Array<string | number> = ["asdf", 1];
 val5.push(2);
 
 // Defining types for tuples
-let val6: [string, number] = ['asdf', 1];
+let val6: [string, number] = ["asdf", 1];
 
 // Defining types for objects
-let user: { name: string, age: number } = {
-    name: 'John',
-    age: 30
+let user: { name: string; age: number } = {
+    name: "John",
+    age: 30,
 };
 
 // Inferring types for objects
 let user2 = {
-    name: 'John',
-    age: 30
+    name: "John",
+    age: 30,
 };
 
 // This is not a object type, but a type that can be any value except null or undefined
-let val7: {} = 'some text';
+let val7: {} = "some text";
 // This is an object type
 let obj = {};
+
+// When do not know the items of an object, we can use the Record utility type
+let data: Record<string, number | string>;
+data = {
+    key1: "value1",
+    key2: 2,
+};
---
+++ choices.ts
@@ -0,0 +1,20 @@
+// There is no enumeration in javascript, but TypeScript provides a way to define enumerations using the `enum` keyword. Enumerations allow you to define a set of named constants, which can be used to represent a collection of related values.
+enum Role {
+    User,
+    Admin,
+    Guest,
+}
+
+let userRole: Role = Role.User;
+let guestRole: Role = Role.Guest;
+
+if (userRole === Role.User) {
+    console.log("User role");
+}
+
+// More used thean enumerations, union types are more flexible and can be used with any type, not just numbers or strings.
+let postStatus: "draft" | "published" | "archived";
+postStatus = "draft";
+
+let someValue: [-1 | 1, "left" | "right"];
+someValue = [1, "right"];
