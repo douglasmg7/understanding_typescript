c201f16d4967539e1e1f05f5137b5447c5d1bcea
douglasmg7 <douglasmg7@gmail.com>
2026-07-21 15:08:15 -0300

    using tsconfig.json and start a new project calculator

---
+++ calculator/calculator.ts
@@ -0,0 +1,10 @@
+
+// Define a type not a value (objec)
+type InvestmentData = {
+    initialAmount: number,
+    annualContribution: number,
+    expetedReturn: number,
+    duration: number,
+};
+
+function calculateInvestment(data: InvestmentData) {}
